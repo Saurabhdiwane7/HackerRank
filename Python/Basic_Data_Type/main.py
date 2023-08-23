26616e198fc82b38e4e797f86d48fb672f9a4402
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-08-24 01:09:23 +0530

    Add files via upload

---
+++ Python/Basic_Data_Type/main.py
@@ -0,0 +1,17 @@
+#Q = Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+# Input Format
+# The first line contains n. The second line contains an array A [] of n integers each separated by a space.
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    unique_arr = list(set(arr))
+    unique_arr.sort(reverse=True)
+
+    result = unique_arr[1]
+    print(result)
+
+
+
