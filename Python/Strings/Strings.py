af538b464855ac1698917ca972880e49b49003f6
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-08-27 17:43:22 +0530

    Add files via upload

--- Python/Strings/Strings.py
+++ Python/Strings/Strings.py
@@ -1,11 +1,10 @@
-#Q = https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+#Q = https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
-def split_and_join(line):
-    line = line.split(" ")
-    line = "-".join(line)
-    return line
 
+def print_full_name(first, last):
+    a =  f"Hello {first} {last}! You just delved into python."
+    print(a)
 if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
