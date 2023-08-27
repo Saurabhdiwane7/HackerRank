1f98d1890c1261baec08af6226e54b0b6a372e5b
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-08-27 18:58:03 +0530

    Add files via upload

---
+++ Python/Strings/Strings -Find A Sub-string.py
@@ -0,0 +1,18 @@
+# Q =https://www.hackerrank.com/challenges/find-a-string/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+def count_substring(string, sub_string):
+    count = 0
+    substring_length = len(sub_string)
+
+    for i in range(len(string) - substring_length + 1):
+        if string[i:i + substring_length] == sub_string:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
