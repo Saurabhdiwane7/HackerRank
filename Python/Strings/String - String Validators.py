99a4e6c4db1e7fb348c0343686b05081876e6686
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-08-27 19:31:18 +0530

    Add files via upload

---
+++ Python/Strings/String - String Validators.py
@@ -0,0 +1,10 @@
+#Q =https://www.hackerrank.com/challenges/string-validators/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+if __name__ == '__main__':
+    s = input().strip()
+    print(any(c.isalnum() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
