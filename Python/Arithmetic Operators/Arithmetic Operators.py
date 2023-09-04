5f72b701223c7a84e0b9ba575c2725b3383c3119
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-04 15:48:41 +0530

    Add files via upload

---
+++ Python/Arithmetic Operators/Arithmetic Operators.py
@@ -0,0 +1,13 @@
+#Q =https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    n = a + b
+    n2 = a - b
+    n3 = a * b
+
+    print(n)
+    print(n2)
+    print(n3)
